3d7e6850189043f4c5ff3d258a3b4139d10ffcb0
maheem_20 <mwistanbuly@gmail.com>
2023-07-14 16:31:06 +0200

    define array of grocery items

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const express = require('express');
 const app = express();
 const ports = process.env.PORT || 3000;
 
+const groceries = [
+    {
+        id: 1,
+        name: 'Milk'
+    },
+    {
+        id: 2,
+        name: 'Eggs'
+    }
+];
+
 app.get('/', (req, res) => {
     res.send('Hello');
 });
