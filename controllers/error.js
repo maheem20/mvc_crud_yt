cefa3c622c7bf2c9acfbab3d331009ff88ed4d3a
maheem_20 <mwistanbuly@gmail.com>
2023-08-17 23:59:32 +0200

    define get404 function

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,7 @@
-exports.get404 = (req, res, next) => { }
+exports.get404 = (req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+}
 
 exports.get500 = (req, res, next) => { }
