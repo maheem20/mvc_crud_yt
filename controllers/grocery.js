7e1de0d0cf7502e33b606ee5056d42267295f31f
maheem_20 <mwistanbuly@gmail.com>
2023-08-17 23:43:32 +0200

    redefine getAllGroceries as asynchronous function

--- controllers/grocery.js
+++ controllers/grocery.js
@@ -1,5 +1,5 @@
 const Grocery = require('../models/grocery');
 
-exports.getAllGroceries = (req, res, next) => {
-    res.send(Grocery.fetchAll());
+exports.getAllGroceries = async (req, res, next) => {
+
 };
