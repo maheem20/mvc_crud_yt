8889e987c83db8c96d9e53d9c4d858cf9d7ea4f0
maheem_20 <mwistanbuly@gmail.com>
2023-08-17 23:55:49 +0200

    mount get404 & get500 middleware from the errorController module

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ const ports = process.env.PORT || 3000;
 
 app.use('/groceries', groceryRoutes);
 
+app.use(errorController.get404);
+
+app.use(errorController.get500);
+
 app.listen(ports, () => console.log(`Server listening on port ${ports}`));
