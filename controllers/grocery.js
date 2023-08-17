e9cad10e08e917970f1fc0c2b58f9d2cc6422464
maheem_20 <mwistanbuly@gmail.com>
2023-08-17 23:48:18 +0200

    send JSON response with retrieved groceries and 200 OK status code

--- controllers/grocery.js
+++ controllers/grocery.js
@@ -3,5 +3,6 @@ const Grocery = require('../models/grocery');
 exports.getAllGroceries = async (req, res, next) => {
     try {
         const [allGroceries] = await Grocery.fetchAll();
+        res.status(200).json(allGroceries);
     }
 };
