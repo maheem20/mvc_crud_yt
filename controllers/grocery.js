32c545bb3461df3adf28f5a7695c13e4b3e2945a
maheem_20 <mwistanbuly@gmail.com>
2023-08-17 23:49:08 +0200

    if error occurs set status code to 500 (Internal Server Error) and pass error to next middleware function

--- controllers/grocery.js
+++ controllers/grocery.js
@@ -4,5 +4,10 @@ exports.getAllGroceries = async (req, res, next) => {
     try {
         const [allGroceries] = await Grocery.fetchAll();
         res.status(200).json(allGroceries);
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
     }
 };
