fd1bbc1ac29251aa3f6ac2228ab06917d36ae354
maheem_20 <mwistanbuly@gmail.com>
2023-08-19 23:25:14 +0200

    move on to next middleware or route handler

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
 });
 
 app.use('/groceries', groceryRoutes);
