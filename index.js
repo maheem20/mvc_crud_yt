fbcefebe93c10a88602dbbfd07a4f6b5b0dad749
maheem_20 <mwistanbuly@gmail.com>
2023-08-19 23:23:47 +0200

    allow requests from any origin

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const ports = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => { });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+});
 
 app.use('/groceries', groceryRoutes);
 
