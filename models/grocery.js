0e2a16a21c1d0cd017c8b9ef947e3dd3fdc0314b
maheem_20 <mwistanbuly@gmail.com>
2023-08-20 23:17:45 +0200

    define method that deletes grocery from database

--- models/grocery.js
+++ models/grocery.js
@@ -17,4 +17,8 @@ module.exports = class Grocery {
     static update(id, item) {
         return db.execute('UPDATE groceries SET item = ? WHERE id = ?', [item, id]);
     }
+
+    static delete(id) {
+        return db.execute('DELETE FROM groceries WHERE id = ?', [id]);
+    }
 };
