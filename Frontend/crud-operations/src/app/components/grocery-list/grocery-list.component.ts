83a33054ae944e4ccb4a72f1ee8ccc15021b6d67
maheem_20 <mwistanbuly@gmail.com>
2023-08-21 23:40:21 +0200

    initialize array of grocery items

--- Frontend/crud-operations/src/app/components/grocery-list/grocery-list.component.ts
+++ Frontend/crud-operations/src/app/components/grocery-list/grocery-list.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grocery-list.component.scss']
 })
 export class GroceryListComponent {
+  groceries = [
+    { id: 1, name: 'Bread' },
+    { id: 2, name: 'Milk' },
+  ];
 
   constructor() { }
 
