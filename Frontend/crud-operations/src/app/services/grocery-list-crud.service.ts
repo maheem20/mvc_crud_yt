fecadd79f8cd0d269a8d76b0374ce509a18015f0
maheem_20 <mwistanbuly@gmail.com>
2023-08-22 22:37:45 +0200

    import Grocery interface model

--- Frontend/crud-operations/src/app/services/grocery-list-crud.service.ts
+++ Frontend/crud-operations/src/app/services/grocery-list-crud.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Grocery } from '../models/Grocery';
+
 @Injectable({
   providedIn: 'root'
 })
