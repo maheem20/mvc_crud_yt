47640ba73d850e8be82b5c233ee3a5bbe4d5abf3
maheem_20 <mwistanbuly@gmail.com>
2023-08-22 23:57:08 +0200

    import Observable & of from rxjs and delete constructor

--- Frontend/crud-operations/src/app/services/error-handler.service.ts
+++ Frontend/crud-operations/src/app/services/error-handler.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorHandlerService {
 
-  constructor() { }
 }
