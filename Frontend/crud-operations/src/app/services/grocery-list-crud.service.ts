abee104ba643840ad3e66a496019fc385d9d1165
maheem_20 <mwistanbuly@gmail.com>
2023-08-23 22:54:45 +0200

    inject ErrorHandlerService into constructor

--- Frontend/crud-operations/src/app/services/grocery-list-crud.service.ts
+++ Frontend/crud-operations/src/app/services/grocery-list-crud.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 
 import { Grocery } from '../models/Grocery';
+import { ErrorHandlerService } from './error-handler.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,10 @@ import { Grocery } from '../models/Grocery';
 export class GroceryListCrudService {
   private url = 'http://localhost:3000/groceries';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private errorHandlerService: ErrorHandlerService,
+    private http: HttpClient
+  ) { }
 
   fetchAll(): Observable<Grocery[]> {
     return this.http.get<Grocery[]>(this.url, { responseType: "json" }).pipe(
