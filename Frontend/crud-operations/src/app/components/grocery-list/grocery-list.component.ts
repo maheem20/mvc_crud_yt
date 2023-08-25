a8e10fbd7275561c3e3ce2f79b42963d543cde55
maheem_20 <mwistanbuly@gmail.com>
2023-08-25 23:26:52 +0200

    import tap from rxjs

--- Frontend/crud-operations/src/app/components/grocery-list/grocery-list.component.ts
+++ Frontend/crud-operations/src/app/components/grocery-list/grocery-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 import { Grocery } from 'src/app/models/Grocery';
 
